f22b54ce650f77ea71b0683ccdccc3a45660a479
Evan Alto <evanalto@Evans-MacBook.local>
2019-05-14 18:14:21 -0500

    minor adjustments

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import './Card.scss'
-import { FaDragon } from 'react-icons/fa'
+import { FaReact } from 'react-icons/fa'
 
 const Card = ({ stage, cardNumber, cardholderName, validThru, securityCode }) => {
-  let displayedCardNumber = cardNumber.value + 'XXXXXXXXXXXXXXXX'.slice(cardNumber.value.length)
-  let cardNumArr = []
-  cardNumArr.push(displayedCardNumber.slice(0, 4).split(''))
-  cardNumArr.push(displayedCardNumber.slice(4, 8).split(''))
-  cardNumArr.push(displayedCardNumber.slice(8, 12).split(''))
-  cardNumArr.push(displayedCardNumber.slice(12, 16).split(''))
+  const displayCardNum = cardNumber.value + 'XXXXXXXXXXXXXXXX'.slice(cardNumber.value.length)
+  const cardNumArr = [ 
+    displayCardNum.slice(0, 4).split(''), 
+    displayCardNum.slice(4, 8).split(''),
+    displayCardNum.slice(8, 12).split(''),
+    displayCardNum.slice(12, 16).split('')
+  ]
   return (
     <div className='card-container'>
       <div className={`card ${stage === 'security-code' ? 'flipped' : ''}`}>
         <div className='face front'>
-          <FaDragon />
-          <div className="card-number">
+          <FaReact />
+          <div className={`card-number ${stage === 'card-number' ? 'current' : 'not'}`}>
           {cardNumArr.map((group,i) => {
             return ( 
-            <span className={`group group-${i}`}key={i}>
-              {group.map(num => {
-                if (num !== 'X') return <span className="num">{num}</span>
-                else return <span>{num}</span>
+            <span className={`group group-${i}`} key={i}>
+              {group.map((num, j) => {
+                if (num !== 'X') return <span key={`${i + j}`} className="num">{num}</span>
+                else return <span key={i + j}>{num}</span>
               })}
             </span>)
             })}
@@ -38,7 +39,7 @@ const Card = ({ stage, cardNumber, cardholderName, validThru, securityCode }) =>
         </div>
         <div className='face back'>
           <div>682</div>
-          <FaDragon />          
+          <FaReact />          
         </div>
       </div>
     </div>
